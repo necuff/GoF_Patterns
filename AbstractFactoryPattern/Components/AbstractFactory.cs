4589bccccd8c462954bc9989252fa9fbd8a10b2c
Mikhail Zaykov <necuff87@gmail.com>
2022-09-08 15:14:25 +0700

    Add abstract factory pattern

---
+++ AbstractFactoryPattern/Components/AbstractFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern.Components
+{
+    abstract class AbstractFactory
+    {
+        public abstract AbstractProductA CreateProductA();
+        public abstract AbstractProductB CreateProductB();
+    }
+}
---
+++ AbstractFactoryPattern/Components/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern.Components
+{
+    class Client
+    {
+        private AbstractProductA _abstractProductA;
+        private AbstractProductB _abstractProductB;
+
+        public Client(AbstractFactory factory)
+        {
+            _abstractProductA = factory.CreateProductA();
+            _abstractProductB = factory.CreateProductB();
+        }
+
+        public void Run()
+        {
+            _abstractProductB.Interact(_abstractProductA);
+        }
+    }
+}
