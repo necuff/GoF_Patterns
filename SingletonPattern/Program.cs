ad215719ed5db1bc87e69753260ac1789b135af9
Mikhail Zaykov <necuff87@gmail.com>
2022-09-09 15:15:21 +0700

    Add singleton pattern

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using SingletonPattern;
+
+Singleton s = Singleton.GetInstance();
+
+Console.WriteLine(s.Name);
+
+s.SetName("This is new name");
+
+Console.WriteLine(s.Name);
+
+Singleton s2 = Singleton.GetInstance();
+
+Console.WriteLine(s2.Name);
+
+s2.SetName("Super new name");
+
+Console.WriteLine(new String('-', 10));
+Console.WriteLine(s.Name);
+Console.WriteLine(s2.Name);
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Singleton
+    {
+        private static Singleton instance = null;
+
+        private Singleton(String name = "Default name")
+        {
+            Name = name;
+        }
+
+        public static Singleton GetInstance()
+        {
+            return instance ?? (instance = new Singleton());
+        }        
+
+        public string Name { get; private set; }
+
+        public void SetName(string newName)
+        {
+            Name = newName;
+        }
+    }
+}
