b10be2e2c433d0ac500362ed836f53e62ebf6108
mzaykov <zaykov@inexika.com>
2022-09-22 11:23:01 +0700

    Add factory method pattern

---
+++ FactoryMethodPattern/Components/ConcreteProductA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPattern.Components
+{
+    public class ConcreteProductA : Product
+    {
+        public override string GetType()
+        {
+            return "Concrete product A";
+        }
+    }
+}
---
+++ FactoryMethodPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿using FactoryMethodPattern.Components;
+
+Creator[] creators = { new ConcreteCreatorA(), new ConcreteCreatorB() };
+
+foreach(Creator creator in creators)
+{
+    Product product = creator.FactoryMethod();
+    Console.WriteLine($"Crated {product.GetType()}");
+}
+
+Console.ReadLine();
